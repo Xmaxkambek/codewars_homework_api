793e43621c86253313a1bbc844aaf0197620e0cc
Xmaxkambek <maxkambekxolbekov515@gmail.com>
2023-01-11 20:32:33 +0500

    the end

--- lib/screens/codewarsPage.dart
+++ lib/screens/codewarsPage.dart
@@ -1,41 +1,40 @@
 import 'package:codewars_homework_api/services/codewars.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xff181818),
-      body: FutureBuilder(
+    return SafeArea(
+      child: Scaffold(
+          body: FutureBuilder(
         future: getCadewars(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
+        builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Center(
-                  child: Text(
-                    'No Internit ',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                )
-              ],
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          } else {
+          } else if (snapshot.hasData) {
             return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                return ListTile(title: snapshot.data[index],);
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: snapshot.data![index],
+                );
               },
             );
           }
+          return Center(
+            child: Text('error'),
+          );
         },
-      ),
+      )),
     );
   }
 }
--- lib/services/codewars.dart
+++ lib/services/codewars.dart
@@ -3,11 +3,13 @@ import 'package:http/http.dart' as http;
 
 Future<Map> getCadewars() async {
   Uri url = Uri.parse(
-      'https://codeschoolhomeworkapi.pythonanywhere.com/codewars/group-all/CodeWars-2023A/');
+      'https://codeschoolhomeworkapi.pythonanywhere.com/codewars/group-all/CodeWars-2023A');
 
   http.Response response = await http.get(
     url,
   );
+  // print(response.statusCode);
+  // print(jsonDecode(response.body).runtimeType);
   Map lesson = jsonDecode(response.body);
   return lesson["problems"];
 }
